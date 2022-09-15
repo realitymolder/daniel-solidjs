6b0eb4bca5b4bd49a0dd704e29383bffb259c62b
Daniel Toubul <molder@toubul.eu>
2022-09-15 15:27:52 +0300

    :zap: first draft for the website

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -5,18 +5,24 @@ import H1Atom from "../atoms/H1Atom";
 const HomePage: Component = () => {
   return (
     <ContainerAtom>
+      <br />
+      <br />
+      <br />
       <H1Atom text="Daniel Toubul" />
-      <p>
-        Security researcher working on mobile privacy/security. Founder and lead
-        developer of GrapheneOS. If you have a good reason to contact me, you
-        can reach me at @strcat:grapheneos.org on Matrix. I prefer being
-        contacted over Matrix, but you can contact me via email at
-        daniel.micay@grapheneos.org. I'm phasing out my usage of obsolete
-        PGP-based technology so you'll need to contact me via Matrix if you want
-        to use end-to-end encryption.
+      <p class="text-lg">
+        <span class="font-bold text-red-500">NOTE:</span> This site is a work in
+        progress.
+        <br /> <span class="font-bold text-red-500">NOTE:</span> Due to effects
+        of the COVID-19-pandemic the site is still in development.
         <br />
         <br />
-        Links: GitHub Linkdin
+        <span class="text-blue-400">
+          You can reach me over{" "}
+          <span class="font-bold">
+            <a href="https://www.linkedin.com/in/danieltoubul/">Linkedin</a> |
+            <a href="https://github.com/realitymolder"> Github</a>
+          </span>
+        </span>
       </p>
     </ContainerAtom>
   );
